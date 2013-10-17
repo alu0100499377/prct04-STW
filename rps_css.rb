55523a976063c29f4721875a31498680dbb8db0a
gabriel garcia <alu0100499377@ull.edu.es>
2013-10-17 18:46:30 +0100

    1ª version

---
+++ rps_css.rb
@@ -0,0 +1,59 @@
+require 'rack/request'
+require 'rack/response'
+require 'haml'
+
+
+module PiedraPapelTijeras
+	class App
+		def initialize(app=nil)
+			@app = app
+			@content_type = :html
+			@defeat = {'piedra' => 'tijeras', 'papel' => 'piedra', 'tijeras' => 'papel'}
+			@throws = @defeat.keys
+		end
+		
+
+		def call(env)
+			req = Rack::Request.new(env)
+
+			req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }
+			
+			computer_throw = @throws.sample
+			player_throw = req.GET["choice"]
+			answer = if !@throws.include?(player_throw)
+				#alert = 0
+				"Elige una de las opciones:"
+			elsif player_throw == computer_throw
+				#alert = 1
+				"Has empatado! :|"
+			elsif computer_throw == @defeat[player_throw]
+				#alert = 2
+				"Vaamos! #{player_throw} gana a #{computer_throw}!! :D"
+			else
+				#alert = 3
+				"Ouu valla! #{computer_throw} gana a #{player_throw}! :("
+			end
+
+			engine = Haml::Engine.new File.open("views/index.haml").read
+			res = Rack::Response.new
+			res.write engine.render({},
+				:answer => answer)
+			res.finish
+		end
+	end
+end
+
+if $0 == __FILE__
+	require 'rack'
+	require 'rack/showexceptions'
+
+	builder = Rack::Builder.new do
+		use Rack::Static, :urls => ['/public']
+		use Rack::ShowExceptions
+		use Rack::Lint
+
+		run PiedraPapelTijeras::App.new
+	end
+
+	Rack::Handler::Thin.run builder
+end
